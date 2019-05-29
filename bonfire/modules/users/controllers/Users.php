05916507ace076036132cc89f04df6967608e692
kuldeep.kumar@wntechs.com <kuldeep.kumar@wntechs.com>
2019-05-29 14:17:34 +0530

    my first commit

--- bonfire/modules/users/controllers/Users.php
+++ bonfire/modules/users/controllers/Users.php
@@ -100,11 +100,7 @@ class Users extends Front_Controller
             }
 
             // If there is nowhere else to go, go home.
-            if($this->session->userdata('role_id')=='1'){
-            Template::redirect('admin/content');
-            }else{
-                Template::redirect('admin/reports/dailly_reports');
-            }
+            Template::redirect('/');
         }
 
         // Prompt the user to login.
